c474fd6e72ad2ee33c363a6909d1872a745e1d76
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-11-03 12:43:19 +0100

    exponentiation done

--- IMPA HT24 O3/exponentiation_748.py
+++ IMPA HT24 O3/exponentiation_748.py
@@ -4,14 +4,17 @@ import sys
 def get_input() -> list[tuple[str, int]]:
     """ returns a list of all rows in the input split into two parts """
     input_lines = sys.stdin.readlines()
-    parsed_input_lines = map(lambda x: tuple(x.split(' ')), input_lines)
+    split_input_lines = map(lambda x: tuple(x.split()), input_lines)
+    parsed_input_lines = map(lambda line: (line[0], int(line[1])), split_input_lines)
     return list(parsed_input_lines)
 
 
 def str2int(number: str) -> tuple[int, int]:
     """ turns a string into a number in the format n * 10^k 
     returns (n, k) """
-    whole, decimal = number.split('.')
+    split_number = number.split('.')
+    whole = split_number[0]
+    decimal = split_number[1] if len(split_number) == 2 else ''
 
     decimal_point = -len(decimal)
     converted_number = int(whole + decimal)
@@ -20,26 +23,30 @@ def str2int(number: str) -> tuple[int, int]:
 
 
 def insert_decimal_point(number: int, decimal_point: int) -> str:
-    """ inserts the decimal point into the number and return result as a string """
+    """ inserts the decimal point into the number and return result as a string 
+    removes trailing zeros and trailing decimal point """
     str_number = str(number)
 
-    leading_zeros = decimal_point - len(str_number)
+    if decimal_point == 0:
+        return str_number
+
+    leading_zeros = -decimal_point - len(str_number)
 
     str_number = leading_zeros * '0' + str_number
 
-    return str.join('.', [str_number[:-decimal_point], str_number[-decimal_point:]])
+    return str.join('.', [str_number[:decimal_point], str_number[decimal_point:]]).rstrip('0').rstrip('.')
 
 
-def solve(number: str, exponent: int) -> None:
-    """ solves the problem for one test case and prints out the answer """
+def solve(number: str, exponent: int) -> str:
+    """ solves the problem for one test case and returns the answer as a string """
     parsed_number, decimal_point = str2int(number)
 
     final_number = parsed_number ** exponent
     final_decimal_point = decimal_point * exponent
 
-    print(insert_decimal_point(final_number, final_decimal_point))
+    return insert_decimal_point(final_number, final_decimal_point)
 
 
 if __name__ == '__main__':
     for number, exponent in get_input():
-        solve(number, exponent)
+        print(solve(number, exponent))
--- IMPA HT24 O3/test_exponentiation_748.py
+++ IMPA HT24 O3/test_exponentiation_748.py
@@ -17,51 +17,83 @@ class TestGetInput(unittest.TestCase):
         mock_stdin.write('123.45 12')
         mock_stdin.seek(0)
 
-        self.assertEqual(get_input(), [('12345', 12)])
+        self.assertEqual(get_input(), [('123.45', 12)])
 
     @patch('sys.stdin', new_callable=io.StringIO)
     def test_multiple(self, mock_stdin: io.StringIO):
         mock_stdin.write('123.45 12\n23.456  1\n.34567 25')
         mock_stdin.seek(0)
 
-        self.assertEqual(get_input(), [('12345', 12), ('23456', 1), ('34567', 25)])
-
-    @patch('sys.stdin', new_callable=io.StringIO)
-    def test_leadin_decimal(self, mock_stdin: io.StringIO):
-        mock_stdin.write('123.45 12\n23.456  1\n.34567 25')
-        mock_stdin.seek(0)
-
-        self.assertEqual(get_input(), [('12345', 12), ('23456', 1), ('34567', 25)])
-
-    @patch('sys.stdin', new_callable=io.StringIO)
-    def test_trailing_decimal(self, mock_stdin: io.StringIO):
-        mock_stdin.write('123.45 12\n23.456  1\n.34567 25')
-        mock_stdin.seek(0)
-
-        self.assertEqual(get_input(), [('12345', 12), ('23456', 1), ('34567', 25)])
-
-    @patch('sys.stdin', new_callable=io.StringIO)
-    def test_leadin_zeros(self, mock_stdin: io.StringIO):
-        mock_stdin.write('123.45 12\n23.456  1\n.34567 25')
-        mock_stdin.seek(0)
-
-        self.assertEqual(get_input(), [('12345', 12), ('23456', 1), ('34567', 25)])
-
-    @patch('sys.stdin', new_callable=io.StringIO)
-    def test_trailing_zeros(self, mock_stdin: io.StringIO):
-        mock_stdin.write('123.45 12\n23.456  1\n.34567 25')
-        mock_stdin.seek(0)
-
-        self.assertEqual(get_input(), [('12345', 12), ('23456', 1), ('34567', 25)])
+        self.assertEqual(get_input(), [('123.45', 12), ('23.456', 1), ('.34567', 25)])
 
 
 class TestStr2Int(unittest.TestCase):
-    pass
+    def test_normal(self):
+        self.assertEqual(str2int('123.45'), (12345, -2))
+
+    def test_leading_zero(self):
+        self.assertEqual(str2int('012.34'), (1234, -2))
+        self.assertEqual(str2int('000.34'), (34, -2))
+        self.assertEqual(str2int('00000.'), (0, 0))
+
+    def test_trailing_zero(self):
+        self.assertEqual(str2int('12.340'), (12340, -3))
+        self.assertEqual(str2int('1.2000'), (12000, -4))
+        self.assertEqual(str2int('.00000'), (0, -5))
+
+    def test_leading_decimal(self):
+        self.assertEqual(str2int('.12345'), (12345, -5))
+
+    def test_trailing_decimal(self):
+        self.assertEqual(str2int('12345.'), (12345, 0))
+
+    def test_no_decimal(self):
+        self.assertEqual(str2int('123456'), (123456, 0))
 
 
 class TestInsertDecimalPoint(unittest.TestCase):
-    pass
+    def test_normal(self):
+        self.assertEqual(insert_decimal_point(123456, -5), '1.23456')
+        self.assertEqual(insert_decimal_point(123456, -4), '12.3456')
+        self.assertEqual(insert_decimal_point(123456, -3), '123.456')
+        self.assertEqual(insert_decimal_point(123456, -2), '1234.56')
+        self.assertEqual(insert_decimal_point(123456, -1), '12345.6')
+
+    def test_leading_decimal(self):
+        self.assertEqual(insert_decimal_point(123456, -6), '.123456')
+
+    def test_trailing_decimal(self):
+        self.assertEqual(insert_decimal_point(123456, 0), '123456')
+
+    def test_leading_zeros(self):
+        self.assertEqual(insert_decimal_point(123, -6), '.000123')
+
+    def test_trailing_zeros(self):
+        self.assertEqual(insert_decimal_point(123000, -3), '123')
 
 
 class TestSolve(unittest.TestCase):
-    pass
+    def test_examples(self):
+        self.assertEqual(solve('95.123', 12), '548815620517731830194541.899025343415715973535967221869852721')
+        self.assertEqual(solve('0.4321', 20), '.00000005148554641076956121994511276767154838481760200726351203835429763013462401')
+        self.assertEqual(solve('5.1234', 15), '43992025569.928573701266488041146654993318703707511666295476720493953024')
+        self.assertEqual(solve('6.7592', 9), '29448126.764121021618164430206909037173276672')
+        self.assertEqual(solve('98.999', 10), '90429072743629540498.107596019456651774561044010001')
+        self.assertEqual(solve('1.0100', 12), '1.126825030131969720661201')
+
+    def test_leading_decimal(self):
+        self.assertEqual(solve('.12345', 5), '.0000' + str(12345**5))
+
+    def test_trailing_decimal(self):
+        self.assertEqual(solve('12345.', 5), str(12345**5))
+
+    def test_leading_zeros(self):
+        self.assertEqual(solve('0001.2', 5), '2.48832')
+        self.assertEqual(solve('000.12', 5), '.0000248832')
+
+    def test_trailing_zeros(self):
+        self.assertEqual(solve('123.00', 5), str(123**5))
+
+
+if __name__ == '__main__':
+    unittest.main()
